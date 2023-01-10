5b278485eb56bdefbfcbeb0e5206a8a1ca8e578a
Alexandr <a.platcev@ya.ru>
2023-01-11 00:25:36 +0300

    add and correct
    add config appveyor and correction error

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,11 +1,11 @@
-import {test} from '@jest/globals';
-import showHealth from "../app.js";
+import { test } from '@jest/globals';
+import showHealth from '../app.js';
 
 test.each([
-    {heroesObj: {name: 'Маг', health: 90}, expected: "healthy"},
-    {heroesObj: {name: 'Маг', health: 45}, expected: "wounded"},
-    {heroesObj: {name: 'Маг', health: 10}, expected: "critical"},
-  ])('inline table', ({heroesObj, expected}) => {
-    const result = showHealth(heroesObj);
-    expect(result).toBe(expected);
-  });
+  { heroesObj: { name: 'Маг', health: 90 }, expected: 'healthy' },
+  { heroesObj: { name: 'Маг', health: 45 }, expected: 'wounded' },
+  { heroesObj: { name: 'Маг', health: 10 }, expected: 'critical' },
+])('inline table', ({ heroesObj, expected }) => {
+  const result = showHealth(heroesObj);
+  expect(result).toBe(expected);
+});
